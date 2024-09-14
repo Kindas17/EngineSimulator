97d6556ba85f3f36eddd7c451008e8aa66dc5d99
Kindas17 <107813993+Kindas17@users.noreply.github.com>
2024-09-14 16:06:28 +0200

    Improving TorqueCurve evaluation library

--- TorquePoverCurve.py
+++ TorquePoverCurve.py
@@ -1,24 +1,31 @@
-from ctypes import CDLL, c_float
+from ctypes import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-SIZE = 30
+STEPS = 40
 
-lib = CDLL("build/libtorqueCurve.so")
-lib.main()
+lib = cdll.LoadLibrary("build/libtorqueCurve.so")
+lib.GetTorqueAtSpeed.restype = c_float
 
-speed_v = np.array(list((c_float * SIZE).in_dll(lib, "speed_v")))
-torque_v = np.array(list((c_float * SIZE).in_dll(lib, "torque_v")))
+speed = 50
+speed_log, torque_log = [], []
+for i in range(STEPS):
+    speed += 12
+    torque = lib.GetTorqueAtSpeed(c_float(speed), c_int(10))
+    speed_log.append(60 * speed / np.pi)
+    torque_log.append(torque)
+    print("{:.2f} Nm @ {:.0f} rpm".format(torque, 60 * speed / np.pi))
 
 fig, axs = plt.subplots(2, sharex=True)
 
-axs[0].plot(speed_v, torque_v)
+axs[0].plot(speed_log, torque_log)
 axs[0].set_xlabel("Engine Speed [RPM]")
 axs[0].set_ylabel("Torque [Nm]")
 axs[0].grid()
 
 axs[1].plot(
-    speed_v, [0.001 * t * (rpm / 60) * 2 * np.pi for t, rpm in zip(torque_v, speed_v)]
+    speed_log,
+    [0.001 * t * (rpm / 60) * 2 * np.pi for t, rpm in zip(torque_log, speed_log)],
 )
 axs[1].set_xlabel("Engine Speed [RPM]")
 axs[1].set_ylabel("Power [kW]")
